52faac7717c69278a733315c331a8c9f88439bcc
Claroja <63183535@qq.com>
2021-03-09 22:38:32 +0800

    自定义登录逻辑2(登录界面)

--- README.md
+++ README.md
@@ -5,8 +5,12 @@
 密码是:看控制台输出
 
 
-2.自定义登录逻辑
+2.自定义登录逻辑(用户验证)
 1)添加登录逻辑service,登录逻辑需要实现UserDetailsService的loadUserByUsername方法.
 方法中可以实现用户的查询,比较和授权,最终返回的是UserDetails接口的实现.
 这里我们就可以自定义用户名和密码了.
 2)在config包中注入了加密算法BCryptPasswordEncoder
+
+
+3.自定义登录逻辑2(登录界面)
+1)http.formLogin()来定义登录界面
---
+++ springSecurityLogin/src/main/java/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @RequestMapping("/success")
+    public String successLogin(){
+        return "success";
+    }
+    @RequestMapping("/failure")
+    public String failureLogin(){
+        return "failure";
+    }
+}
